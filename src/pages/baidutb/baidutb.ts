7da80f64066c9b14f00211ab7295f1cc40275cc4
杨月 <83718701@qq.com>
2017-12-14 14:13:46 +0800

    1209

--- src/pages/baidutb/baidutb.ts
+++ src/pages/baidutb/baidutb.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+///<reference path="../../../node_modules/ionic-angular/tap-click/tap-click.d.ts"/>
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the BaidutbPage page.
@@ -13,16 +14,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-baidutb',
   templateUrl: 'baidutb.html',
 })
+
 export class BaidutbPage {
   gender;
   items = [
-    {tx_img:'assets/img/user.png',name:'张三',floor:'第5楼',time:'今天 10：20',nr_src:'assets/img/speakers/mouse.jpg',nr:'测试测试测试测试测试测试测试测试测试测试测试'},
-    {tx_img:'assets/img/user.png',name:'李四',floor:'第4楼',time:'今天 10：10',nr_src:'',nr:'测试测试测试测试'},
-    {tx_img:'assets/img/user.png',name:'王萨吉',floor:'第3楼',time:'今天 10：00',nr_src:'',nr:'测试测试测试测试'},
-    {tx_img:'assets/img/user.png',name:'尚雯',floor:'第2楼',time:'今天 9：40',nr_src:'',nr:'测试测试测试测试测试测试测试测试'},
-    {tx_img:'assets/img/user.png',name:'老王',floor:'第1楼',time:'今天 9：20',nr_src:'',nr:'测试测试测试测试测试测试测试'}
+    {
+      tx_img: 'assets/img/user.png',
+      name: '张三',
+      floor: '第5楼',
+      time: '今天 10：20',
+      nr_src: 'assets/img/speakers/mouse.jpg',
+      nr: '测试测试测试测试测试测试测试测试测试测试测试'
+    },
+    {tx_img: 'assets/img/user.png', name: '李四', floor: '第4楼', time: '今天 10：10', nr_src: '', nr: '测试测试测试测试'},
+    {tx_img: 'assets/img/user.png', name: '王萨吉', floor: '第3楼', time: '今天 10：00', nr_src: '', nr: '测试测试测试测试'},
+    {tx_img: 'assets/img/user.png', name: '尚雯', floor: '第2楼', time: '今天 9：40', nr_src: '', nr: '测试测试测试测试测试测试测试测试'},
+    {tx_img: 'assets/img/user.png', name: '老王', floor: '第1楼', time: '今天 9：20', nr_src: '', nr: '测试测试测试测试测试测试测试'}
   ]
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  isShow=false;
+  mousedownd() {
+    this.isShow = true
+  }
+
+  mouseup() {
+    this.isShow = false
+  }
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams) {
+
   }
 
   ionViewDidLoad() {

e6a239b6e84b4408b436ecdeaac0c6df49c02675
zhangrui95 <zhangrui_web@163.com>
2017-12-14 15:48:24 +0800

    change

--- src/pages/decision/decision.ts
+++ src/pages/decision/decision.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {UsersPage} from "../users/users";
 
-/**
- * Generated class for the DecisionPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-decision',
@@ -16,14 +9,26 @@ import {UsersPage} from "../users/users";
 })
 export class DecisionPage {
 
+  issue = '若你作为辉发乳业（集团）股份有限公司的决策者，关注到网贴后该如何决策？';
+  options = [
+    {id:'0',option:'A', text:'公开调查帖子内容的真实性'},
+    {id:'1',option:'B', text:'私下联系发帖人，删除网帖，控制消息的网络传播渠道'},
+    {id:'2',option:'C', text:'公开调查帖子内容的真实性'}
+  ]
+  Id;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DecisionPage');
+  getForm(item){
+    this.Id = item.id;
   }
+
   getUser(){
     this.navCtrl.push(UsersPage);
   }
 
+  vote(){
+    console.log(this.Id);
+  }
 }

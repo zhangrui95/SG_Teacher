03892662a4a4f4598cdaee5389de3e4ac50ce775
zhangrui95 <zhangrui_web@163.com>
2017-12-15 16:25:30 +0800

    add

--- src/pages/operation/operation.ts
+++ src/pages/operation/operation.ts
@@ -9,6 +9,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OperationPage {
   showBtn = false;
+  list = [
+    {name:'点名',style:{top:'0px',left:'10px'},bgShow:false},
+    {name:'讨论',style:{top:'10px',left: '110px'},bgShow:false},
+    {name:'事件',style:{bottom:'110px',right: '10px'},bgShow:true},
+    {name:'角色',style:{bottom:'10px',right: '0px'},bgShow:false}
+    ]
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -24,4 +30,10 @@ export class OperationPage {
     }
   }
 
+  showBg(index){
+    for(let i in this.list){
+      this.list[i].bgShow = false;
+    }
+    this.list[index].bgShow = true;
+  }
 }
--- src/pages/rsidebar/rsidebar.ts
+++ src/pages/rsidebar/rsidebar.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the RsidebarPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-rsidebar',
   templateUrl: 'rsidebar.html',
 })
 export class RsidebarPage {
+  list = [
+    {name:'分组',img:'assets/img/group.png',bgColor: true,key:false},
+    {name:'继续',img:'assets/img/go.png',bgColor: false,key:false},
+    {name:'详情',img:'assets/img/detail.png',bgColor: false,key:false},
+    {name:'投屏',img:'assets/img/view.png',bgColor: false,key:false},
+    {name:'',img:'assets/img/key.png',bgColor: false,key:true},
+  ]
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RsidebarPage');
+  }
+  bgChange(index){
+    for(let i in this.list){
+      this.list[i].bgColor = false;
+    }
+    this.list[index].bgColor = true;
   }
 
 }

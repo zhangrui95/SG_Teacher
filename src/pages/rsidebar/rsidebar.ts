06517a4740c87503ed8f1e2c9e54c54347cf591f
zhangrui95 <zhangrui_web@163.com>
2018-01-05 15:05:21 +0800

    add

--- src/pages/rsidebar/rsidebar.ts
+++ src/pages/rsidebar/rsidebar.ts
@@ -7,13 +7,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'rsidebar.html',
 })
 export class RsidebarPage {
-  list = [
-    {type:'group',name:'分组',img:'assets/img/group.png',bgColor: true,key:false},
-    {type:'next',name:'继续',img:'assets/img/go.png',bgColor: false,key:false},
-    {type:'detail',name:'详情',img:'assets/img/detail.png',bgColor: false,key:false},
-    {type:'screen',name:'投屏',img:'assets/img/view.png',bgColor: false,key:false},
-    {type:'',name:'',img:'assets/img/key.png',bgColor: false,key:true},
-  ]
+  // list = [
+  //   {type:'group',name:'分组',img:'assets/img/group.png',bgColor: true,key:false},
+  //   {type:'next',name:'继续',img:'assets/img/go.png',bgColor: false,key:false},
+  //   {type:'detail',name:'详情',img:'assets/img/detail.png',bgColor: false,key:false},
+  //   {type:'screen',name:'投屏',img:'assets/img/view.png',bgColor: false,key:false},
+  //   {type:'',name:'',img:'assets/img/key.png',bgColor: false,key:true},
+  // ]
+  showList = false;
   @Output()
   onNext= new EventEmitter<string>();
 
@@ -22,18 +23,11 @@ export class RsidebarPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad RsidebarPage');
   }
-  bgChange(index){
-    if(this.list[index].type=='next'){
-      this.onNext.emit("next")
-    }
-    if(this.list[index].type=='screen'){
-      this.onNext.emit("screen")
-    }
-
-    for(let i in this.list){
-      this.list[i].bgColor = false;
-    }
-    this.list[index].bgColor = true;
+  groupListShow(){
+    this.showList = true;
+  }
+  groupListHide(){
+    this.showList = false;
   }
 
 }

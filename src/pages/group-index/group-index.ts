8f7cb9ab2674f5314613fcdbb23a4eca8ad16364
杨月 <83718701@qq.com>
2018-01-13 17:09:22 +0800

    修改各场景页面

--- src/pages/group-index/group-index.ts
+++ src/pages/group-index/group-index.ts
@@ -1,6 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {ServerSocket} from "../../providers/ws.service";
+import {Subscription} from "rxjs/Subscription";
 
 
 @IonicPage()
@@ -8,9 +9,24 @@ import {ServerSocket} from "../../providers/ws.service";
   selector: 'page-group-index',
   templateUrl: 'group-index.html',
 })
-export class GroupIndexPage implements OnInit{
+export class GroupIndexPage implements OnInit,OnDestroy{
+  @Input()
+  s_data: any = new Object();
+  @Input()
+  sim_id: any = new Object();
+  datas;
+  n_id;
+  g_id;
+
+  title;
+  content;
+  txt_length;
+
+  private socketSubscription: Subscription
+
   ngOnInit(): void {
-    console.log(this.s_data)
+    console.log(this.s_data.s_data.componentList)
+    this.datas = this.s_data.s_data.componentList;
     this.ws.connect();
     if(this.ws.messages){
       console.log(this.ws.messages)
@@ -19,19 +35,41 @@ export class GroupIndexPage implements OnInit{
         console.log(res)
       })
     }
-
-
   }
 
-  @Input()
-  s_data :any=new Object();
-  @Input()
-  sim_id :any=new Object();
-  constructor(public navCtrl: NavController, public navParams: NavParams,public ws :ServerSocket) {
-
+  ngOnDestroy(){
+    if (this.socketSubscription)
+      this.socketSubscription.unsubscribe();
   }
 
+  constructor(public navCtrl: NavController, public navParams: NavParams ,public ws: ServerSocket) {
+  }
 
+  getData() {
+    this.datas = this.s_data.s_data.componentList[0].data.fillData;
 
+    // this.title = 'jdsiajdoi';
+    // this.content = '一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十';
+  }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad GroupIndexPage');
+  }
+  // ionViewDidEnter() {
+  //   if (this.ws.messages) {
+  //     this.socketSubscription = this.ws.messages.subscribe((message: string) => {
+  //       console.log('received message from server11111:' + message);
+  //       if (JSON.parse(message)['action'] != null) {
+  //         if (JSON.parse(message)['action'] == 'phone_scene_answers_update') {
+  //           this.items = JSON.parse(message)['list']
+  //         }
+  //       }
+  //     })
+  //   }
+  // }
+
+  // ionViewDidLeave() {
+  //   if (this.socketSubscription)
+  //     this.socketSubscription.unsubscribe();
+  // }
 }
